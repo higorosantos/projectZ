a0f6c1052e16253875961c7c5c78222976f92e80
Higor Oliveira <higorosantos@gmail.com>
2020-03-02 18:27:23 -0300

    Dale
    Pastas e subpastas

---
+++ Projeto Z/Assets/Teste.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cubo;
+    public GameObject inicio;
+    public GameObject fim;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void OnTriggerEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("inicio"))
+        {
+            Debug.Log("ok");
+            cubo.transform.Translate(new Vector3(0.08f, 0, 0));
+        }
+        if (collision.gameObject.CompareTag("fim"))
+        {
+            cubo.transform.Translate(new Vector3(-0.08f, 0, 0));
+        }
+    }
+    void Update()
+    {
+        cubo.transform.Translate(new Vector3(-0.08f,0,0));
+    }
+}
